ac392b533ab6ebf0f5c8381b39f79b7ad5a84950
Akshath Sivaprasad <akshath@berkeley.edu>
2018-07-21 04:46:59 -0700

    i am a padding, updated content, a/an switch, selectric menu max height

--- assets/js/aki.js
+++ assets/js/aki.js
@@ -45,16 +45,19 @@ $('#rotate-text').Morphext({
 
 /* I am a... section */
 $(() => {
-    $('select').selectric();
+    $('select').selectric({
+        maxHeight: 200
+    });
 })
 
+var vowels = 'aeiou';
 $(function() {
     $('.i-am-a-subtext').hide();
-    $('#who-am-i-journalist').show();
-
+    $('#who-am-i-activist').show();
     $('#who-am-i-selector').on("change", function() {
         $('.i-am-a-subtext').hide();
         $('#who-am-i-'+$(this).val()).show();
-    }).val("journalist");
+        $('.i-am-a-text').text((vowels.indexOf($(this).val()[0]) !== -1) ? 'I am an' : 'I am a');
+    }).val("activist");
 });
 
