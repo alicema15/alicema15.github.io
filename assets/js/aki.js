4a438cd6e4f1b196f56a608a4ef6225dad4a22ff
Akshath Sivaprasad <akshath@berkeley.edu>
2018-07-20 18:42:24 -0700

    js hotfix + styling

--- assets/js/aki.js
+++ assets/js/aki.js
@@ -34,22 +34,6 @@ $('#rotate-text').Morphext({
 });
 
 /* Public data section */
-function openSection(event, sectionId) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName('tab-content-c');
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = 'none';
-    }
-    tablinks = document.getElementsByClassName('tab-link-c');
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(' active', '');
-    }
-    document.getElementById(sectionId).style.display = 'block';
-    event.currentTarget.className += ' active';
-}
-
-document.getElementById('default-open').click();
-
 window.onscroll = () => {
     if (window.scrollY > 0) {
         $('.main-header').addClass('scrolled')
