3801e48b4ebc78092147f01a6d2f36acbf82e436
Akshath Sivaprasad <akshath@berkeley.edu>
2018-07-20 18:23:15 -0700

    added rotating img

--- assets/js/aki.js
+++ assets/js/aki.js
@@ -1,3 +1,39 @@
+/* Banner image */
+// $("#banner-slideshow > img:gt(0)").hide();
+
+// setInterval(function() { 
+//   $('#banner-slideshow > img:first')
+//     .fadeOut(1500)
+//     .next()
+//     .fadeIn(1500)
+//     .end()
+//     .appendTo('#banner-slideshow');
+// },  3000);
+
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("banner-image");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none"; 
+    }
+    slideIndex++;
+    if (slideIndex > x.length) {slideIndex = 1} 
+    x[slideIndex-1].style.display = "block"; 
+    setTimeout(carousel, 2500);
+}
+
+/* Banner text */
+$('#rotate-text').Morphext({
+    animation: "fadeInUp",
+    separator: ",",
+    speed: 2500,
+    complete: function() {}
+});
+
+/* Public data section */
 function openSection(event, sectionId) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName('tab-content-c');
@@ -11,12 +47,5 @@ function openSection(event, sectionId) {
     document.getElementById(sectionId).style.display = 'block';
     event.currentTarget.className += ' active';
 }
-
-$('#rotate-text').Morphext({
-    animation: "fadeInUp",
-    separator: ",",
-    speed: 2500,
-    complete: function() {}
-});
 
 document.getElementById('default-open').click();
